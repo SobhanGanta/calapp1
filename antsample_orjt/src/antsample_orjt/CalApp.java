809824ba071cb5310dfca3710127351966416235
training <training@PC233266.cts.com>
2016-08-11 13:39:10 +0530

    first commit

---
+++ antsample_orjt/src/antsample_orjt/CalApp.java
@@ -0,0 +1,11 @@
+package antsample_orjt;
+
+public class CalApp {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		System.out.println("Multipication is::" + a * b);
+	}
+
+}
